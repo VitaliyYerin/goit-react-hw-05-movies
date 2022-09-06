9ff39284aa4c0b09ad340dee509a0de588e5f3e7
Vitaliy Yerin <37743681+VitaliyYerin@users.noreply.github.com>
2022-09-06 13:12:05 +0300

    change App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
+import { lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navigation from './Navigation/Navigation';
+
+const Home = lazy(() =>
+  import('pages/HomePage' /* webpackChunkName: "home-page" */)
+);
+const Movies = lazy(() =>
+  import('pages/MoviesPage' /* webpackChunkName: "movies-page" */)
+);
+const MovieDetailsPage = lazy(() =>
+  import(
+    'pages/MovieDetailsPage/MovieDetailsPage' /* webpackChunkName: "movie-detalis-page" */
+    )
+);
+const Cast = lazy(() =>
+  import('./Cast/Cast' /* webpackChunkName: "cast-page" */)
+);
+const Reviews = lazy(() =>
+  import('./Reviews/Reviews' /* webpackChunkName: "reviews-page" */)
+);
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Navigation />
+      <Suspense fallback={<div>Loading Films...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="movies/:movieId/*" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </>
   );
 };
